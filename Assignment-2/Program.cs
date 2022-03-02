892b3765a7f32c7279c7aed617ef57fff556d648
Ashish <sam4lordjesus@outlook.com>
2022-03-02 22:53:00 +0530

    start assignment2

---
+++ Assignment-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TaskAB;
+using TaskC;
+using System.Drawing;
+using Google.Cloud.Vision.V1;
+
+namespace Program
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if(args.Length==0)
+            {
+                Console.WriteLine("Need additional arguments for the program!");
+            }
+            else
+            {
+                if(args[0].ToUpper()=="C" && args.Length>= 2)
+                {
+                    string path = args[1];
+                    Image img1 = Image.FromFile(path);
+                }
+                else
+                {
+                    Console.WriteLine("Undefined arguments!");
+                }
+            }
+            Console.WriteLine("\nThis program features the following modes\nB \t\t\t: Task B\nC <image_path>\t\t: Task C");
+        }
+    }
+}
