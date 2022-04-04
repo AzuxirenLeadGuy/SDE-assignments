da889529a2e993ee791e83158d1307bdd050bb1b
Ashish <sam4lordjesus@outlook.com>
2022-04-04 13:05:22 +0530

    more changes

--- SDE_A3_M21CS003/Controllers/ImagePredictController.cs
+++ SDE_A3_M21CS003/Controllers/ImagePredictController.cs
@@ -1,17 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Google.Cloud.Storage.V1;
-using Google.Cloud.AIPlatform.V1;
 using Google.Apis.Auth.OAuth2;
-using Google.Cloud.AIPlatform.V1.Schema.Predict.Instance;
-using Google.Cloud.AIPlatform.V1.Schema.Predict.Params;
 using Google.Cloud.Vision.V1;
-using System.Diagnostics;
 namespace SDE_A3_M21CS003.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class ImagePredictController : ControllerBase
 {
+    public const string JsonCredentials = "{\"type\": \"service_account\",\"project_id\": \"a3-iitj\",\"private_key_id\": \"d15eab7b7e0603b159bd1bf901b9f8be18f90220\",\"private_key\": \"-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCSuyH2A5M7FS5k\nScGyg9GSUIBA1KCguThIphyt/pcWW2jCWxLvWxFkryxojplHK7saKMNQTClIg+CV\nlfxDSx9wFHxNNP5Y+3tDJq+11sR4MiA/vvuPbOzQvMUn4haEz5qJDfW/f4i3FlkI\n+XzGJRUylI3XwFh5uVMVfzbt2oqAV/TI86ZbHJj2x/Oc5rq2tm1oZVrX2AWmleDu\nwCtKU3CpyiPbBcvUZO8rm0FvkC6MI3CjmmoOCK8CMg+3spDavqhInViO+9Bvu24i\nFQxEYI7yMazloOqDOKiViarnK90nPM9vucLaSkbPXnjeUC1UB8J9/xw9T5SH5MXm\nHvAkFn1RAgMBAAECggEAA+WVYs+4fsayuzYcP03x6GVb6pzbATm6DW/f7h0HTTAz\nSYoaqCGuhhKC5un2lyXJq8muWjkTlZnvAnn/v5fEp3jbzj8cjCdtoBxsaTNhd2G5\naof7bVUyut5wi/0cpP1QeNhWkPXlnxsoVS0vC6IbfILme3bfwqfR1Y3R2yJFMGBb\nT12qaDZF4TSsKDGbPIi8NvdLl87sE74Qrvusm2B7KdtrITOiNs0YXcP2Hp+ZBNiL\nTd+rNQdTnEnyYEucSU83f/30NiYG9Qhcws+oRxozqRfBwHgfh3s8Q4U7dSnXaCl4\ntYwww9mvO/3ZPoKgQuKlXE2NQTbiU+xZsHMLFS1SiwKBgQDNqBlVIsmf8dQOscSR\nLP1nsQaeG1ncZsxx/dmbdpQfoEuYIThbDPJdnbxkmGPaZbSMw0ee3pKT8yxlT/eW\nRiCVysZKsddjAaZXrSJ0Si9JIecnHXj1SB5t/xD26MPAyv9Uj3t9v+9dWARFJgju\n6DWyew+gkj5BGW6M2VTFKiKcmwKBgQC2plXT7Bun1P7fRoHsn9zyo6jJE32pxsoI\nV7u/QRdp/dzUJaZm7x1h2IqG23rsnHV8oqAl6ESEkxhlBPcfzfrWOtmZIzFa9k8R\nuYA4/wycLqhimHIWrVkSP8QWQudm26IH7UZjGiRUz8+wBtcgMZbAX3bdlk7yra6K\nH29phz2ugwKBgDnf43kk+A6t1FpqRoIAYjOrCsy1r70ppMEzieiJHzgjOQLP3ncp\nFo+n0xuQExGj64JtdhtMUX2XQlgkf+1nQMZGsTTWBWIUVcZ4r6iAM/xlsRI95gtI\nx193F0QUzvnTZrVaR4tk1yyyl0kKYhsxd+MJZKVwYlqZmJXEl2mA6QqfAoGAEacV\n2HuTWK8vSukHmZUBx7mlkHl/xa7ey9ue26cw2h+c/iv9XylxD4ncKZiA0ul0OW2G\nSOOf66hDzU+jisFfouDhMvnBxWbU0YO6LsgHsfiYcZ8GN8bdOvRKwVQKBVjo2hdG\nnEFkjYXgLsUaeZQRW9peJKBoVPAQVG0hWXOmtH0CgYA6bLoA/Xn91Y4zOZS1YxVp\n/wAld4Fw0YIG0nUIkS8/G8JAVVIkg+vRITBNyAlBVtdmdahsxdDP5xMvtWwEoEVM\n+/P9IlJeh6vREwDEwkprYrC5kgW77aOXbdI00305SypTUnbd45GurO4x9B1Ogs6m\nPnJ+6mR4vksakJZNnrtsFQ==\n-----END PRIVATE KEY-----\n\",\"client_email\": \"ashish@a3-iitj.iam.gserviceaccount.com\",\"client_id\": \"112924551724206061423\",\"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\",\"token_uri\": \"https://oauth2.googleapis.com/token\",\"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\",\"client_x509_cert_url\": \"https://www.googleapis.com/robot/v1/metadata/x509/ashish%40a3-iitj.iam.gserviceaccount.com\"}";
     readonly Random random;
     private readonly ImageAnnotatorClientBuilder builder;
     public ImagePredictController()
@@ -19,25 +16,43 @@ public class ImagePredictController : ControllerBase
         random = new Random();
         builder = new() { JsonCredentials = JsonCredentials };
     }
-    public readonly static GoogleCredential credentials = GoogleCredential.FromJson(JsonCredentials);
+    public readonly GoogleCredential credentials = GoogleCredential.FromJson(JsonCredentials);
     internal static string MachineName = "";
-    [HttpPost(Name = "GetPrediction")]
-    public ResultObject Post()
+    [HttpPost]
+    public ResultObject Post(string keywords = "")
     {
-        string bucketName = "a3-bucket";
+        if (keywords == null || keywords == string.Empty) return new(MachineName, null!);
+        var streamcopy = new MemoryStream();
+        Request.Body.CopyToAsync(streamcopy);
+        streamcopy.Seek(0, SeekOrigin.Begin);
+        var img = Image.FromStreamAsync(streamcopy);
+        string bucketName = "sde-a3-bucket";
         string imagename = "image-a3-" + random.NextInt64().ToString();
-        Image img = Image.FromStreamAsync(Request.Body).Result;
         var storage = StorageClient.Create(credentials);
-        storage.UploadObject(bucketName, imagename, null, Request.Body);
-        Stream downstream = new MemoryStream();
-        storage.DownloadObject(bucketName, imagename, downstream);
         ImageAnnotatorClient client = builder.Build();
-        var obj = new ResultObject(client.DetectFaces(img).ToArray(), client.DetectLogos(img).ToArray()) { MachineName = MachineName };
+        var obj = new ResultObject(MachineName, client.DetectText(img.Result));
+        foreach (var keyword in keywords.Split(';', StringSplitOptions.None))
+        {
+            bool found = false;
+            string word = keyword.ToUpper();
+            foreach (var txt in obj.Texts)
+            {
+                string sentence = System.Text.RegularExpressions.Regex.Replace(txt.Description.ToUpper(), @"[^\x20-\x7F]", "");
+                if (sentence.Contains(word))
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (found == false) return obj;
+        }
+        streamcopy.Seek(0, SeekOrigin.Begin);
+        var file = storage.UploadObject(bucketName, imagename, null, streamcopy);
+        obj.Uploaded = true;
+        obj.UploadURL = file.Name;
         return obj;
     }
-    public const string JsonCredentials = "{\"type\": \"service_account\", \"project_id\": \"assignment3-344904\", \"private_key_id\": \"e28eec3faca68cbe3a15a58f3d1ee062718a962e\", \"private_key\": \"-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDIoUVzxJ5cq1CU\nRnxOsqXLZCK37VW/53gu+VNboidqevMQv0Hlwu7GnHqGWHGFGQeJTDgczLkIrqrK\nF3fWOFbJKHF85H1Qr5JrS5slLjzJEKl65YD2tSYpHXWj0g/jDYjDvrvH5IomKB5+\nHZujcFukMqc5Kkn1t1YaRiJ8om4BGI8z5c6Dt60HcHWR2FWLfcozAnb18SwUy9UL\nBBGy3KL44watkPRF1vyrAAIGCaaI+r7z6tV8i5UsmaY+MGwy8o+25lUsdbz9FUMq\nzf3dB+79ta/7POA3SvTbprb5NcJK8IPFSP999sReBIev2PP5BQpoJyKrAIPkY0A5\nUiZYfmF1AgMBAAECggEADyxF0LPdoiEr+Tq8wJrkIlX9W4RDHZNHfzid5D/cKUJe\ndYrGqgtW/DBDV+ZXwiVwG3hnL05lmVF/BcTxQYMwlQqjopAJJiamX8pE/nKDvapZ\nQZEacrilHSuRhQUoQcH5RjcmLmo17x14841mWDLxdfaFYl5fX4ow+dBfX/uiGXT8\nCgvTRk5gTfIEVgFcmhklHWKgxGqPNwB9oBMD+br78TEgBeFSDTQqs0nhMpEAwyYX\nNq2UvLEPTx89O3ITBtkXXBHfyE8qTFcYdS4tqhFFseZfw+yoIXLpjh64sDEsWpKu\nOCndXGIi5qt41fQlCS2XNmme01gT7h1fqqb8RJohwQKBgQDud2eMe48e3VxlVIR7\nl70p+r7j2YYWRKngl54mOarqfx+yL3ds4AFQjttCuP6KbFmqZ12cp/XG+CAMgP6d\nNxl4Ol702h2CYfBMXRwNi6Ydm+ouhdPXbnl/jJEijY9aa6FBE+VZQb19UFxVnWZr\n2mSEEfqBwnKuQXpJ03fK+pgzpQKBgQDXYa4sb1dEnSPwsmJv8Lw94ha7z022/LA0\nOlecIhzRnuEji1bOzd2qfDgS+EpzlW518MPu+PVBtGLstPvB0RePxgQn24hahHvR\nNFg2LAHYYWU4KuPxn0EKpdOdMD2/6oQgpUxuUZlMqK1Vp6azKapNEpbCXOll0axI\npGue/9bNkQKBgHEzv7VBxoxJWx5DQ+0D+GmgPdQeI/gYOmiCsTHGOGu/U0WcIN5P\nbHBYkJ+ZAESn8Nzi2t6OIS+GBHjLF5N8X5viYcdsb4vAgHlPzgxkGcR+imnxX8Iq\nEIVLLm6vsrii1Vb2Ye7ANXrylhKSeNeEQikr+7Zzn0s3jBzVxU1XLr99AoGAH289\nOXUCmpwIq8+NOGM9RhbiVITcBdH5cZX2RW5hIGL83vAuOj6nlINOt84PJ7Fujr4W\nCqHBQdau/Xcoq0/2DWDMKHkM/JELdSwbl8RyfH8yg7EW1aP1nTU4nl91E7aJlC4r\n6on0QtD3g2HVnZ5+IYOOB04CLn8ckvmUT39tfVECgYEA1hBeXGObaBf54GW8jy86\nXO6N41ugH9gInMntl2bwE71zXF3yFLKV1WeJX28udn30M5rIw+Tx4gFV60wC1R7C\n0AKN+v25ztymY9n2f36AHrMemr+SU1ZVBCwSDntEuiq+I0a5EMJKimoFWcDDI86t\n4g1Y0SEz4okXdDqZhp2SelI=\n-----END PRIVATE KEY-----\n\", \"client_email\": \"ajsam-14@assignment3-344904.iam.gserviceaccount.com\", \"client_id\": \"107141962401276170929\", \"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\", \"token_uri\": \"https://oauth2.googleapis.com/token\", \"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\", \"client_x509_cert_url\": \"https://www.googleapis.com/robot/v1/metadata/x509/ajsam-14%40assignment3-344904.iam.gserviceaccount.com\" }";
 }
-
 
 /// <summary>
 /// The structure of the result returned by this API
@@ -46,106 +61,25 @@ public class ResultObject
 {
     public string MachineName { get; set; }
     /// <summary>
-    /// Collection of faces in the image
+    /// Collection of texts in the image
     /// </summary>
     /// <value></value>
-    public FaceObject[] Faces { get; set; }
+    public EntityAnnotation[] Texts { get; set; }
     /// <summary>
-    /// Collection of logos in the image
+    /// True if the object is also uploaded to the cloud
     /// </summary>
     /// <value></value>
-    public LogoObject[] Logos { get; set; }
+    public bool Uploaded { get; set; }
     /// <summary>
-    /// Construuctor for this class
+    /// The URL of the file in the bucket, if file is indeed uploaded to the bucket
     /// </summary>
-    private ResultObject()
+    /// <value></value>
+    public string UploadURL { get; set; }
+    public ResultObject(string mac, IEnumerable<EntityAnnotation> list)
     {
-        MachineName = "";
-        Faces = Array.Empty<FaceObject>();
-        Logos = Array.Empty<LogoObject>();
-    }
-    /// <summary>
-    /// Construuctor for this class
-    /// </summary>
-    public ResultObject(FaceAnnotation[] facesAnn, EntityAnnotation[] logosAnn)
-    {
-        MachineName = "";
-        Faces = new FaceObject[facesAnn.Length];
-        Logos = new LogoObject[logosAnn.Length];
-        int i;
-        for (i = facesAnn.Length - 1; i >= 0; i--)
-        {
-            Faces[i] = new(facesAnn[i]);
-        }
-        for (i = logosAnn.Length - 1; i >= 0; i--)
-        {
-            Logos[i] = new(logosAnn[i]);
-        }
-    }
-    /// <summary>
-    /// object returned for invalid input or any other exception
-    /// </summary>
-    public static ResultObject Default => new();
-}
-/// <summary>
-/// An object denoting a face in the image
-/// </summary>
-public class FaceObject
-{
-    /// <summary>
-    /// The confidence value for this portion of the image is a Face
-    /// </summary>
-    /// <value></value>
-    public float Confidence { get; set; }
-    /// <summary>
-    /// How likely is this face happy
-    /// </summary>
-    /// <value></value>
-    public string JoyLiklihood { get; set; }
-    /// <summary>
-    /// How likely is this face Sad
-    /// </summary>
-    /// <value></value>
-    public string SadLiklihood { get; set; }
-    /// <summary>
-    /// How likely is this face angry
-    /// </summary>
-    /// <value></value>
-    public string AngryLiklihood { get; set; }
-    /// <summary>
-    /// Constructor for this class
-    /// </summary>
-    /// <param name="face"></param>
-    public FaceObject(FaceAnnotation face)
-    {
-        Confidence = face.DetectionConfidence;
-        JoyLiklihood = Enum.GetName(face.JoyLikelihood) ?? "?";
-        SadLiklihood = Enum.GetName(face.SorrowLikelihood) ?? "?";
-        AngryLiklihood = Enum.GetName(face.AngerLikelihood) ?? "?";
-    }
-}
-/// <summary>
-/// An object denoting a Logo in the image
-/// </summary>
-public class LogoObject
-{
-    /// <summary>
-    /// The confidence value for this portion of image is Logo
-    /// </summary>
-    /// <value></value>
-    public float Confidence { get; set; }
-    /// <summary>
-    /// Description of this logo
-    /// </summary>
-    /// <value></value>
-    public string Description { get; set; }
-    /// <summary>
-    /// Constructor for this class
-    /// </summary>
-    /// <param name="logo"></param>
-    public LogoObject(EntityAnnotation logo)
-    {
-        Confidence = logo.Score / 10.0f;
-        Description = logo.Description;
+        MachineName = mac;
+        Texts = list.ToArray();
+        Uploaded = false;
+        UploadURL = null!;
     }
 }
--- SDE_A3_M21CS003/Readme.md
+++ SDE_A3_M21CS003/Readme.md
@@ -8,7 +8,7 @@ Submitted by: Ashish Jacob Sam (M21CS003)
 
 This assignment requires to use the following GCP(Google Cloud Platform) services in an application and showcase it.
 - Storage Services: Offers limitless storage on cloud. 
-- Cloud AI: Offers cloud based execution of ML tasks
+- Cloud AI: Offers cloud based execution of ML tasks and Artificial intelligence
 - Computing Service: Service for preparing a VM for hosting a web-server or for preforming computations.
 
 For this, the following application use case is defined
@@ -22,4 +22,62 @@ Google Compute Engine allows to prepare/launch VMs on cloud. For this assignment
 
 Configure the VM instance to support http traffic as mentioned in the [official documentation](https://cloud.google.com/vpc/docs/firewalls). Also for the specific C# project, in the file `Properties/launchSetting.json`, edit the IP addresses and port as mentioned in this [StackOverflow answer](https://stackoverflow.com/a/65381082/6488350), and publish the application using the command line argument `--urls` to specify the port number 80.
 
-Now the web can be accessed at the given external IP address as shown
+Now the web can be accessed at the given external IP address as shown:
+
+Next, we can set up a instance group following the guide [here](https://cloud.google.com/compute/docs/tutorials/high-availability-load-balancing).
+
+The following is used as a start-up script for the instances:
+
+```bash
+#! /bin/bash
+
+# install the dependancies
+sudo apt install -y screen git curl make
+
+wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-6.0
+  
+# All dependancies are installed.
+
+git clone https://AzuxirenLeadGuy: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@github.com/AzuxirenLeadGuy/SDE-assignments.git
+cd SDE-assignments/SDE_A3_M21CS003
+screen -dmS webapp
+screen -S webapp -p 0 -X stuff 'make && make run\n'
+```
+
+This script will install dotnet sdk and clone a specified repository to run and build the application
+
+## Google Cloud Store
+
+A bucket is prepared to store the files for the web application, named as 'sde-a3-bucket' For setting it up, [this guide](https://cloud.google.com/storage/docs/reference/libraries) was referred.
+
+Storing a file can be done by simply using the following script:
+
+```csharp
+using Google.Apis.Storage.v1.Data;
+using Google.Cloud.Storage.V1;
+using System;
+
+public class CreateBucketSample
+{
+    public Bucket CreateBucket(
+        string projectId = "your-project-id",
+        string bucketName = "your-unique-bucket-name")
+    {
+        var storage = StorageClient.Create();
+        var bucket = storage.CreateBucket(projectId, bucketName);
+        Console.WriteLine($"Created {bucketName}.");
+        return bucket;
+    }
+}
+```
+
+## Cloud AI
+
+Google offers multiple subpackages in its umbrella service for AI, including Vertex AI, Vision AI and AutoML. For this assignment, the Vision AI API is selected, specifically to read and identify characters from the image.  
