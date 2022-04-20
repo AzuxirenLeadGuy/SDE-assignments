0812869c2bb3f64dc5347402fc56ce9dca0489f1
Ashish <sam4lordjesus@outlook.com>
2022-04-20 11:38:43 +0530

    standardized tests

--- Project/Program.cs
+++ Project/Program.cs
@@ -7,11 +7,11 @@ namespace SDE_Project
     {
         public static void Main()
         {
-            GenericTest(32, 2, 5);
-            GenericTest(64, 3, 10);
-            GenericTest(1024, 5, 20);
-            GenericTest(8192, 6, 24);
-            GenericTest(65536, 6, 24);
+            GenericTest(32, 2, 10);
+            GenericTest(64, 3, 20);
+            GenericTest(1024, 5, 40);
+            GenericTest(8192, 6, 64);
+            GenericTest(65536, 6, 128);
         }
         public struct BloomFilterResult
         {
@@ -23,8 +23,8 @@ namespace SDE_Project
             HABF<int> hashbloomfilter = new(bitsize, ksize, extrahashsize);
             Random randomGen = new();
             StandardBloomFilter<int> normalfilter = new(bitsize, ksize, (byte)randomGen.Next(ksize, extrahashsize));
-            int insertionsize = (int)(bitsize * 0.69f / ksize);
-            IOrderedEnumerable<int> allitems = Enumerable.Range(0, insertionsize).OrderBy(x => randomGen.Next());
+            int insertionsize = (int)(bitsize * 0.69f / ksize) * 2;
+            int[] allitems = Enumerable.Range(0, 65536).OrderBy(x => randomGen.Next()).Take(insertionsize).ToArray();
             double sum = insertionsize;
             insertionsize /= 2;
             int[] poskeys = allitems.Take(insertionsize).ToArray();
@@ -34,8 +34,17 @@ namespace SDE_Project
             for (i = 0; i < poskeys.Length; i++)
             {
                 normalfilter.Insert(poskeys[i]);
-                hashbloomfilter.Add(poskeys[i], randomGen.Next(3) == 0);
+                hashbloomfilter.Add(poskeys[i]);
             }
+            int failed = 0;
+            for (i = 0; i < negkeys.Length; i++)
+            {
+                if (hashbloomfilter.OptimizeFalsePositiveKey(negkeys[i]) == false)
+                {
+                    failed++;
+                }
+            }
+            Console.WriteLine($"Optimization failed for {failed}/{poskeys.Length} items!");
             for (i = 0; i < poskeys.Length; i++)
             {
                 if (normalfilter.Check(poskeys[i]))
@@ -45,7 +54,7 @@ namespace SDE_Project
                 if (hashbloomfilter.Check(poskeys[i]))
                     b.TruePositive++;
                 else
-                    b.TrueNegative++;
+                    b.FalseNegative++;
             }
             for (i = 0; i < negkeys.Length; i++)
             {
